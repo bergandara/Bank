79be156c5cb505dba8bea0d31cf99f03557c1176
bergandara <gandaraber@gmail.com>
2024-02-02 00:17:39 +0000

    OpenAccount class created
    OpenAccount inherits Transaction

---
+++ src/OpenAccount.java
@@ -0,0 +1,11 @@
+public class OpenAccount extends Transaction{
+    public OpenAccount(int customerId, int tellerId){
+        super(customerId, tellerId);
+    }
+
+    @Override
+    public String getTransactionDescription(){
+        return "Teller " + getTellerId() + " opened account " + getCustomerId();
+    }
+    
+}

cf130d93dd1a06240ab1f5c7b78526cc62cc0a46
bergandara <gandaraber@gmail.com>
2024-02-02 00:35:45 +0000

    bankAccount class created

---
+++ src/BankAccount.java
@@ -0,0 +1,23 @@
+public class BankAccount {
+    private int customerId;
+    private String name;
+    private int balance;
+
+    public BankAccount(int customerId, String name, int balance) {
+        this.customerId = customerId;
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public int getBalance(){
+        return this.balance;
+    }
+
+    public void deposit (int amount){
+        this.balance += amount;
+    }
+
+    public void withdraw(int amount){
+        this.balance -= amount;
+    }
+}
